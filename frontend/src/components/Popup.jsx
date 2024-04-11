154da9a7d3ac34c7340b7e5f09e5f6a32912b96a
omkar-afk <omkardeshmukh7890@gmail.com>
2024-04-12 00:22:20 +0530

    added title and desc

---
+++ frontend/src/components/Popup.jsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { RxCross1 } from "react-icons/rx";
+import { baseURL } from "../utils/constant";
+
+const Popup = ({ setShowPopup, popupContent, setUpdateUI }) => {
+  const [input_title, setInput_title] = useState(popupContent.title);
+  const [input_desc, setInput_desc] = useState(popupContent.desc);
+
+  const updateToDo = () => {
+    axios
+      .put(`${baseURL}/update/${popupContent.id}`, { title: input_title, description: input_desc })
+      .then((res) => {
+        console.log(res.data);
+        setUpdateUI((prevState) => !prevState);
+        setShowPopup(false);
+      });
+  };
+
+  return (
+    <div className="backdrop">
+      <div className="popup">
+        <RxCross1 className="cross" onClick={() => setShowPopup(false)} />
+        <h1>Update ToDo</h1>
+
+        <div className="popup__input_holder">
+          <input
+            value={input_title}
+            onChange={(e) => setInput_title(e.target.value)}
+            type="text"
+            placeholder="Update ToDo..."
+          />
+          <input
+            value={input_desc}
+            onChange={(e) => setInput_desc(e.target.value)}
+            type="text"
+            placeholder="Update ToDo..."
+          />
+          <button onClick={updateToDo}>Update</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
---
+++ frontend/src/components/ToDo.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import React from "react";
+import { AiFillEdit } from "react-icons/ai";
+import { RxCross1 } from "react-icons/rx";
+import { baseURL } from "../utils/constant";
+
+const ToDo = ({ title, desc, id, setUpdateUI, setShowPopup, setPopupContent }) => {
+  const deleteTodo = () => {
+    axios.delete(`${baseURL}/delete/${id}`).then((res) => {
+      console.log(res.data);
+      setUpdateUI((prevState) => !prevState);
+    });
+  };
+
+  const updateToDo = () => {
+    setPopupContent({ title, desc, id });
+    setShowPopup(true);
+  };
+
+  return (
+    <div className="toDo">
+      <div>
+      <p>{title}</p>
+      <p>{desc}</p>
+      </div>
+      
+      <div className="icons">
+        <AiFillEdit className="icon" onClick={updateToDo} />
+        <RxCross1 className="icon" onClick={deleteTodo} />
+      </div>
+    </div>
+  );
+};
+
+export default ToDo;
